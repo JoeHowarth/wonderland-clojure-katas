8d80473db11d04325c2c49a81f2ea4ced29fdde0
JoeHowarth <josephehowarth@gmail.com>
2017-05-19 14:37:58 -0400

    didn't commit test file...

--- card-game-war/test/card_game_war/game_test.clj
+++ card-game-war/test/card_game_war/game_test.clj
@@ -2,18 +2,25 @@
   (:require [clojure.test :refer :all]
             [card-game-war.game :refer :all]))
 
+(defn winner-helper [p1-card p2-card]
+  (get (play-round p1-card p2-card) 0))
 
 ;; fill in  tests for your game
 (deftest test-play-round
   (testing "the highest rank wins the cards in the round"
-    (is (= 0 1)))
-  (testing "queens are higher rank than jacks")
-  (testing "kings are higher rank than queens")
-  (testing "aces are higher rank than kings")
-  (testing "if the ranks are equal, clubs beat spades")
-  (testing "if the ranks are equal, diamonds beat clubs")
-  (testing "if the ranks are equal, hearts beat diamonds"))
+    (is (= 2 (winner-helper [:spade 4] [:heart 6]))))
+  (testing "queens are higher rank than jacks"
+    (is (= 2 (winner-helper [:spade :jack] [:heart :queen]))))
+  (testing "kings are higher rank than queens"
+    (is (= 2 (winner-helper [:spade :queen] [:heart :king]))))
+  (testing "aces are higher rank than kings"
+    (is (= 1 (winner-helper [:spade :ace] [:heart :king]))))
+  (testing "if the ranks are equal, clubs beat spades"
+    (is (= 1 (winner-helper [:club :king] [:spade :king]))))
+  (testing "if the ranks are equal, diamonds beat clubs"
+    (is (= 2 (winner-helper [:club 5] [:diamond 5]))))
+  (testing "if the ranks are equal, hearts beat diamonds"
+    (is (= 1 (winner-helper [:heart :king] [:diamond :king])))))
 
 (deftest test-play-game
   (testing "the player loses when they run out of cards"))
-
