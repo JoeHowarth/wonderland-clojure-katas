6f09c546f2803a28edb7d8de66280d377abc5904
JoeHowarth <josephehowarth@gmail.com>
2017-07-28 21:13:55 -0400

    precalculate same length words in dict

--- doublets/src/doublets/solver.clj
+++ doublets/src/doublets/solver.clj
@@ -21,22 +21,27 @@
 (defn numDiff [w1 w2]
   (reduce sameLetter 0 (map vector w1 w2)))
 
-(defn candidates [w1 links]
+(defn candidates [w1 links dict]
   (filter
     #(= 1 (numDiff w1 %1)) ;; find all 1 letter different words
-    (filter
-      #(= (count w1) (count %1))
-      (set/difference (set words) (set links)))))
+    (set/difference (set dict) (set links)))) ; don't backtrack
+
+(defn words-sameLen [w1]
+  (filter
+    #(= (count w1) (count %1))
+    words))
+
 
 (defn doublets
   ([w1 w2]
-   (into [] (doublets w1 w2 []))) ; if no coll, call with empty
-  ([w1 w2 links]
+   (into []
+     (doublets w1 w2 [] (words-sameLen w1)))) ; if no coll, call with empty
+  ([w1 w2 links dict]
    (if (= w1 w2)
     (into links [w1])
     (some
-      #(doublets %1 w2 (into links [w1]))
-      (candidates w1 links)))))
+      #(doublets %1 w2 (into links [w1]) dict)
+      (candidates w1 links dict)))))
 
 ; (defn doublets
 ;   ([w1 w2]
